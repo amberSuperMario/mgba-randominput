c605dda7f0cefdb5ba453bc5fc2cb11af20da451
Amber <96128507+amberSuperMario@users.noreply.github.com>
2022-08-06 00:23:24 -0700

    Bug fixes, random intervals, and cleanup
    Made it so the script now waits for a random amount of frames before pressing a button, and also holds the button for a random amount of frames before releasing it. Removed an unnecessary callback that caused the random inputs to not be correctly handled. Added a seed to the RNG so that the script does not play the same set of inputs every time it's loaded. Added comments and cleaned up variables a bit.

--- randominput.lua
+++ randominput.lua
@@ -1,22 +1,40 @@
+local delay = 0
 local random_input = 0
+local wait_time = 0
+local hold_time = 0
+local max_wait_time = 60 --The script randomly decides how long to wait before pressing a button, between 1 frame and the number of frames listed here
+local max_hold_time = 180 --Same as above but for how long the button is held
+
+math.randomseed(os.time())
+math.random() --Seed the script so the same inputs don't happen every time
 
 function randomInput()
-    random_input = math.random(0,7)
-    if random_input == 2 then
+    random_input = math.random(0,7) --Currently excluding 8 and 9 to disable triggers, see "Constants" @ https://mgba.io/docs/scripting.html
+    if random_input == 2 then --Skips Select inputs
         randomInput()
     end
 end
 
-local delay = 0
+function waitTime()
+    wait_time = math.random(1,max_wait_time) --How long to wait before pressing a button, in frames
+end
+
+function holdTime()
+    hold_time = wait_time + math.random(1,max_hold_time) --How long to hold a button before releasing it, in frames
+end
 
 function updateBufferKeysRead()
-    if delay == 20 then
+    if delay == wait_time then
         emu:addKey(random_input)
     end
-    if delay == 22 then
+    if delay == hold_time then
         emu:clearKey(random_input)
         randomInput()
         delay = 0
+    end
+    if delay == 0 then
+    waitTime()
+    holdTime()
     end
     delay = delay + 1
 end
@@ -24,6 +42,5 @@ end
 function updateBufferFrame()
 end
 
-callbacks:add("frame", randomInput)
 callbacks:add("frame", updateBufferFrame)
 callbacks:add("keysRead", updateBufferKeysRead)
